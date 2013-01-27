f80584733c096489f31f4e252f419073112a25b9
Muthu Nagu <muthunagu100@gmail.com>
2013-01-26 21:59:54 -0500

    first commit

---
+++ src/WelcomeMK.java
@@ -0,0 +1,12 @@
+public class WelcomeMK{
+        public static void main(String args[]){
+                String[] strarr = new String[3];
+                strarr[0] = "Hello";
+                strarr[1] = "Everyone!";
+                strarr[2] = "How are you?";
+
+                for (String printStr : strarr){
+                        System.out.print(printStr + " ");
+                }
+        }       
+}
